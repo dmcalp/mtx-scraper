59035233d7aa58f6f7be9989da938014d45ea141
Dan <d-mcalpine@outlook.com>
2021-12-07 01:40:38 +0000

    clarity

--- scraper.py
+++ scraper.py
@@ -5,13 +5,14 @@ URL = "https://www.pathofexile.com/shop/category/daily-deals"
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
 page = requests.get(URL, headers = headers)
 soup = BeautifulSoup(page.content, 'html.parser')
+
 names = soup.find_all('a', {"class": "name"})
 prices = soup.find_all('div', {"class": "price"})
 
+
 print("============ Todays deals ============", '\n')
 
-for i in range(len(names)):
-    print(names[i].get_text(), prices[i].get_text())
+for name, price in zip(names, prices):
+    print(name.text, price.text)
 
-print()
-print(URL)
+print('\n', URL)
